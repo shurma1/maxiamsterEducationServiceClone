100988df01d14754a668bbbb35d8d2c3e10d2125
shurma1 <dmitriy.prokhorov.04@mail.ru>
2025-04-08 17:53:51 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import routes from "./routes";
+import cors from 'cors';
 import {errorHandlingMiddleware} from "./middleware/errorHandling.middleware";
 
 const app = express();
+
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type'],
+}));
 
 app.use(express.json());
 app.use('/service', routes);
